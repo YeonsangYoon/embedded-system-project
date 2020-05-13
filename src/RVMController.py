33d6d8e9f4f9754b338ad26b3820d0cb4f90299c
yoon <1004dustkd@gmail.com>
2020-05-13 20:49:53 +0900

    Add RVMController and UI debug test code

---
+++ src/RVMController.py
@@ -0,0 +1,253 @@
+"""
+This is RVM controller
+
+Components:
+
+    1. User Interface
+    2. Main Cycle
+    3. RVM status
+
+main cycle:
+
+    1. machine stat check
+    2. initial condition check
+    3. rail move command
+    4. 판별 요청 
+    5. 분류기 작동
+    6. 최종 이동
+    7. update status
+
+"""
+
+import requests
+import threading
+import os
+import sys
+import time
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+#import Serial
+#import RPI.GPIO as GPIO
+
+# machine stat
+RVM_STATE_OFF = 0
+RVM_STATE_ON = 1
+
+# execute Type
+EXEC_NONE_TYPE = 0
+EXEC_INITCOND_TYPE = 1
+EXEC_RAIL_TYPE = 2
+EXEC_IMAGEPROCESS_TYPE = 3
+EXEC_ROUTING_TYPE = 4
+
+# Error Type
+retValOK = 1
+Error = -1
+
+class RVM_Stat: 
+
+    def __init__(self):
+
+        self.machine_stat = RVM_STATE_OFF
+        self.exec_stat = 0
+        self.recycling_number = {
+            "can" : 0,
+            "pet" : 0
+        }
+        self.error_stat = 0
+
+    def startRVM(self):
+        self.machine_stat = RVM_STATE_ON
+    
+    def terminationRVM(self):
+        self.machine_stat = RVM_STATE_OFF
+
+    def updateStatus(self):
+        if self.machine_stat == RVM_STATE_OFF:
+            self.exec_stat = EXEC_NONE_TYPE
+            self.recycling_number['can'] = 0
+            self.recycling_number['pet'] = 0
+        else:
+            self.exec_stat = EXEC_NONE_TYPE
+
+    def putTrashIntoRVM(self, result):
+        if result == 'can':
+            self.recycling_number['can'] += 1
+        elif result == 'pet':
+            self.recycling_number['pet'] += 1
+
+
+# user interface class
+# UI파일 연결
+# 단, UI파일은 Python 코드 파일과 같은 디렉토리에 위치해야한다.
+form_class1 = uic.loadUiType("untitled.ui")[0]
+form_class2 = uic.loadUiType("phoneNumber.ui")[0]
+
+# 화면을 띄우는데 사용되는 Class 선언
+class mainWindow(QMainWindow,form_class1) :
+    can_num = 0
+    pet_num = 0
+    phone_number = ""
+    def __init__(self) :
+        super().__init__()
+        self.setupUi(self)
+        self.pbtn_test_c.clicked.connect(self.test_c)
+        self.pbtn_test_p.clicked.connect(self.test_p)
+        self.pbtn_next_page.clicked.connect(self.next_page)
+
+    def ready(self) :
+        self.phone_number=phone_window.current_PN
+        self.can_num=0
+        self.pet_num=0
+        self.label_intro.setText("쓰레기를 넣어 주세요.\nphone number : "+self.phone_number)
+
+    def test_c(self) :
+        self.can_num +=1
+        self.label_can_num.setText(str(self.can_num)+'개')
+
+    def test_p(self) :
+        self.pet_num +=1
+        self.label_pet_num.setText(str(self.pet_num)+'개')
+
+    def next_page(self) :
+        phone_window.ready()
+        main_window.close()
+        phone_window.show()
+        
+
+class phoneWindow(QMainWindow, form_class2) :
+    current_PN = ""
+    def __init__(self) :
+        super().__init__()
+        self.setupUi(self)
+
+        self.button_0.clicked.connect(lambda: self.buttonClick(0))
+        self.button_1.clicked.connect(lambda: self.buttonClick(1))
+        self.button_2.clicked.connect(lambda: self.buttonClick(2))
+        self.button_3.clicked.connect(lambda: self.buttonClick(3))
+        self.button_4.clicked.connect(lambda: self.buttonClick(4))
+        self.button_5.clicked.connect(lambda: self.buttonClick(5))
+        self.button_6.clicked.connect(lambda: self.buttonClick(6))
+        self.button_7.clicked.connect(lambda: self.buttonClick(7))
+        self.button_8.clicked.connect(lambda: self.buttonClick(8))
+        self.button_9.clicked.connect(lambda: self.buttonClick(9))
+        self.button_bs.clicked.connect(self.bsClick)
+        self.button_enter.clicked.connect(self.enClick)
+
+    def ready(self) :
+        self.current_PN=""
+        self.phone_number.setText(str(self.current_PN))
+
+    def buttonClick(self, num) :
+        self.current_PN = self.current_PN + str(num)
+        self.phone_number.setText(str(self.current_PN))
+
+    def bsClick(self) :
+        self.current_PN = self.current_PN[:-1]
+        self.phone_number.setText(str(self.current_PN))
+
+    def enClick(self) :
+        phone_window.close()
+        main_window.ready()
+        main_window.show()
+
+
+def main_Cycle():
+    # main cycle
+    while 1:
+
+        print("#1 : check condition....")    #debug
+        time.sleep(1)
+        if RVM_status.machine_stat == RVM_STATE_ON:        #1 machine stat check
+
+            #2 initial condition check
+            retval = checkObjectCond()
+            if retval < 0:
+                errorExit(1)
+
+            #3 rail move command 
+            retval = moveCommand('Dzone')
+            if retval < 0:
+                errorExit(1)
+
+            #4 판별 요청
+            #resultD = requests.get("URL")
+            resultD = requestD()
+
+            # 분류기 작동
+            retval = moveCommand(resultD)
+            if retval < 0:
+                errorExit(1)
+
+            # 스탯 업데이트
+            RVM_status.updateStatus()
+
+            #debug msg
+            print("debug msg : 1 trash complete")
+
+def errorExit(errtype):
+    
+    print("ERROR %s : bulabula", errtype)
+
+def checkObjectCond():
+    #debug code
+    print("#2 : check object condition.....")
+    time.sleep(2)
+    return retValOK
+
+def moveCommand(destination):
+    if destination == 'can':
+        print("#5 : moving to can zone.....")
+        time.sleep(3)
+        return retValOK
+
+    elif destination == 'pet':
+        print("#5 : moving to pet zone.....")
+        time.sleep(3)
+        return retValOK
+
+    elif destination == 'Dzone':
+        print("#3 : moving to discriminating zone.....")
+        time.sleep(3)
+        return retValOK
+
+    elif destination == 'return':
+        print("#5 : moving to entrance.....")
+        time.sleep(3)
+        return retValOK
+
+    else:
+        return Error
+
+def requestD():
+    #response = requests.get("URL")
+    response = 'pet'
+    print("#4 : discriminating image....")
+    time.sleep(4)
+    return response 
+
+####################################################################################
+#                                   main sequence                                  #
+####################################################################################
+
+# stat class init
+RVM_status = RVM_Stat() 
+
+ 
+#시리얼 통신 인터페이스 init
+
+
+# 유저 인터페이스 init
+app = QApplication(sys.argv) 
+
+phone_window = phoneWindow() 
+main_window = mainWindow()
+phone_window.show()
+
+t1 = threading.Thread(target = main_Cycle)
+t1.daemon = False
+
+t1.start()
+
+app.exec_()
+
