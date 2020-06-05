bde8ac3aee43c1b8f616c84aba0c8ecf2f9d5f4e
Tae Young Yoo <tae_young.yoo@stud-mail.uni-wuerzburg.de>
2020-06-05 20:42:27 +0900

    Now run_yolo.py satisfies all the required featrues. Capture - run Yolo - get result

--- src/run_yolo.py
+++ src/run_yolo.py
@@ -2,17 +2,49 @@ import os
 import subprocess
 import time
 
-cmd_yolo = "./darknet detector test data/obj.data cfg/yolov3.cfg backup/yolov3_last.weights rvm/image/test_1.jpg"
+cmd_capture = "nvgstcapture-1.0 --cus-prev-res=1920x1080 -A blahblahblah yeon goo sil ga seo sseun dang"
+cmd_yolo = "./darknet detector test data/obj.data cfg/yolov3.cfg backup/yolov3_last.weights rvm/image/test_1.jpg >> detect.txt"
+
+def capture_vid():
+    try:
+        subprocess.call(cmd_yolo, shell=True, timeout=4)
+    except subprocess.TimeoutExpired:
+        print("Timeout during execution")
 
 def test_yolo():
     try:
         subprocess.call(cmd_yolo, shell=True, timeout=10)
     except subprocess.TimeoutExpired: 
-        print("Timeout during execution")   
+        print("Timeout during execution")
 
+def parse_result(file_name, threshold):
+    f = open(file_name)
+    lines = f.readlines()
+    lines = lines[1:]
+    can = []
+    pet = []
+    for i in range(len(lines)):
+        lines[i] = lines[i].replace(":", "")
+        lines[i] = lines[i].replace("%\n", "")
+        each = lines[i].split()
+        if(each[0] == 'pet'):
+            pet.append(int(each[1]))
+        else:
+            can.append(int(each[1]))
+    can_possibility = sum(pet)/len(pet)
+    pet_possibility = sum(pet)/len(pet)
+    #0 is idle, 1 is pet, 2 is can
+    if (max(can_possibility, pet_possibility) < threshold):
+        return 0, 0
+    elif(can_possibility>pet_possibility):
+        return 1, pet_possibility
+    else:
+        return 2, can_possibility
 
 def main():
-    test_yolo2()
+    capture_vid()
+    test_yolo()
+    petOrCan, possibility = parse_result('D:\workspace_py\darknet_parse\parse_1.txt', 50)
 
 if __name__ == '__main__':
     main()
